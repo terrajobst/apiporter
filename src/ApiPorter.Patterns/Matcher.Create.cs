61514c1ada97f102f57d4ef294cf28029110099a
Immo Landwerth <immo@landwerth.net>
2015-11-10 21:33:30 -0800

    Remove suerfluous usings

--- src/ApiPorter.Patterns/Matcher.Create.cs
+++ src/ApiPorter.Patterns/Matcher.Create.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
